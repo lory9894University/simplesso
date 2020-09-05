0c42021b02b1e9a3fb1ffc1c53604161256fd29e
antonio de cesare <antonio.decesare@gmail.com>
2020-09-05 20:25:15 +0200

    solo un test

--- Matrice.java
+++ Matrice.java
@@ -4,146 +4,146 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 class Matrice {
-  private double[][] matrice = new double[3][5];
-  private double[] coefficientiNoti = new double[3];
-  private double[] funzioneObiettivo = new double[5];
-  NumberFormat nf = new DecimalFormat("##.###");
+    private double[][] matrice = new double[3][5];
+    private double[] coefficientiNoti = new double[3];
+    private double[] funzioneObiettivo = new double[5];
+    NumberFormat nf = new DecimalFormat("##.###");
 
-  public Matrice(double[][] matrice, double[] coefficientiNoti, double[] funzioneObiettivo) {
-    this.matrice = matrice;
-    this.coefficientiNoti = coefficientiNoti;
-    this.funzioneObiettivo = funzioneObiettivo;
-  }
-
-  public Matrice(){
-    Scanner keyboard = new Scanner(System.in);
-
-    System.out.println("inizializzo funzione obiettivo");
-    System.out.print("x1:\t");
-    funzioneObiettivo[0] = keyboard.nextInt();
-    System.out.print("x2:\t");
-    funzioneObiettivo[1] = keyboard.nextInt();
-    System.out.println();
-
-    System.out.println("inizializzo la matrice");
-    for (int i = 0; i < matrice.length; i++) {
-      System.out.println("riga " + (i + 1));
-      for (int j = 0; j < matrice[i].length; j++) {
-        System.out.print("colonna: " + (j + 1) + "\t");
-        matrice[i][j] = keyboard.nextInt();
-      }
-      System.out.print("coefficiente " + (i + 1) + "\t");
-      coefficientiNoti[i] = keyboard.nextInt();
-      System.out.println();
-
-    }
-  }
-
-  public int trovaVariabileEntrante() {
-    int result = -1;
-    double max = -1;
-
-    for (int i = 0; i < funzioneObiettivo.length; i++) {
-      if (funzioneObiettivo[i] > 0 && funzioneObiettivo[i] > max) {
-        max = funzioneObiettivo[i];
-        result = i;
-      }
+    public Matrice(double[][] matrice, double[] coefficientiNoti, double[] funzioneObiettivo) {
+        this.matrice = matrice;
+        this.coefficientiNoti = coefficientiNoti;
+        this.funzioneObiettivo = funzioneObiettivo;
     }
 
-    if (result > -1) System.out.println("result " + result);
-    else System.out.println("end!");
+    public Matrice() {
+        Scanner keyboard = new Scanner(System.in);
 
-    return result;
-  }
+        System.out.println("inizializzo funzione obiettivo");
+        System.out.print("x1:\t");
+        funzioneObiettivo[0] = keyboard.nextInt();
+        System.out.print("x2:\t");
+        funzioneObiettivo[1] = keyboard.nextInt();
+        System.out.println();
 
-  public void cambioBase (int rigaPivot, int colonnaPivot){
+        System.out.println("inizializzo la matrice");
+        for (int i = 0; i < matrice.length; i++) {
+            System.out.println("riga " + (i + 1));
+            for (int j = 0; j < matrice[i].length; j++) {
+                System.out.print("colonna: " + (j + 1) + "\t");
+                matrice[i][j] = keyboard.nextInt();
+            }
+            System.out.print("coefficiente " + (i + 1) + "\t");
+            coefficientiNoti[i] = keyboard.nextInt();
+            System.out.println();
+
+        }
+    }
+
+    public int trovaVariabileEntrante() {
+        int result = -1;
+        double max = -1;
+
+        for (int i = 0; i < funzioneObiettivo.length; i++) {
+            if (funzioneObiettivo[i] > 0 && funzioneObiettivo[i] > max) {
+                max = funzioneObiettivo[i];
+                result = i;
+            }
+        }
+
+        if (result > -1) System.out.println("result " + result);
+        else System.out.println("end!");
+
+        return result;
+    }
+
+    public void cambioBase(int rigaPivot, int colonnaPivot) {
     /*if (matrice[rigaPivot][colonnaPivot]==0){
       FIXME:caso posssibile ma mai incontrato in un esercizio. se lo trovo lo includo
     }*/
-    double valoreMoltiplicativo;
-    double valorePivot = matrice[rigaPivot][colonnaPivot];
+        double valoreMoltiplicativo;
+        double valorePivot = matrice[rigaPivot][colonnaPivot];
 
-    if (matrice[rigaPivot][colonnaPivot]<0) {
-      for (int i = 0; i < matrice[rigaPivot].length; i++) {
-        matrice[rigaPivot][i]=-matrice[rigaPivot][i];
-      }
-    }
-    //riduzione della riga del pivot in modo che quest'ultimo sia 1
-    if (matrice[rigaPivot][colonnaPivot]!=1) {
-      for (int i = 0; i < matrice[rigaPivot].length; i++) {
-        matrice[rigaPivot][i] = matrice[rigaPivot][i] == 0 ? 0 : matrice[rigaPivot][i] / valorePivot;
-        coefficientiNoti[rigaPivot] = coefficientiNoti[rigaPivot] == 0 ? 0 : coefficientiNoti[rigaPivot] / valorePivot;
-      }
-    }
-    // riduzione delle altre 2 righe per ottenere un vettore linearmente indipendente (porto a zero i valori aventi
-    // la stessa colonna del pivot)
-    for (int i = 0; i < matrice.length ; i++) {
-      if (i==rigaPivot) //questo procedimento non va ovviamente eseguito per la riga del pivot
-        continue;
-      valoreMoltiplicativo = matrice[i][colonnaPivot]/1; // diviso 1 per sapere se mettere il segno meno
-      for (int j = 0; j <matrice[i].length ; j++) {
-        matrice[i][j]= matrice[i][j]-valoreMoltiplicativo*matrice[rigaPivot][j];
-        coefficientiNoti[i]=coefficientiNoti[i]-valoreMoltiplicativo*coefficientiNoti[rigaPivot];
-      }
-    }
-
-    this.aggiornaFunzioneObbiettivo();
-  }
-
-  public void aggiornaFunzioneObbiettivo(){
-    //TODO:da scrivere
-  }
-
-  /*TODO: da testare*/
-  public int[] trovaVariabileUscente() {
-    System.out.println("uno");
-    int[] elemento = {-1, trovaVariabileEntrante()};
-
-    if (elemento[1] > -1) {
-      System.out.println("colonna " + elemento[1]);
-
-      double minimo = Double.MAX_VALUE;
-      for (int i = 0; i < matrice.length && elemento[1] > -1; i++) {
-        double valoreI =  matrice[i][elemento[1]] / coefficientiNoti[i];
-        if (valoreI > 0 && valoreI < minimo) {
-          System.out.println("i " + i + " valoreI " + valoreI);
-          minimo = valoreI;
-          elemento[1] = i;
+        if (matrice[rigaPivot][colonnaPivot] < 0) {
+            for (int i = 0; i < matrice[rigaPivot].length; i++) {
+                matrice[rigaPivot][i] = -matrice[rigaPivot][i];
+            }
         }
-        System.out.println("minimo " + minimo);
-      }
+        //riduzione della riga del pivot in modo che quest'ultimo sia 1
+        if (matrice[rigaPivot][colonnaPivot] != 1) {
+            for (int i = 0; i < matrice[rigaPivot].length; i++) {
+                matrice[rigaPivot][i] = matrice[rigaPivot][i] == 0 ? 0 : matrice[rigaPivot][i] / valorePivot;
+                coefficientiNoti[rigaPivot] = coefficientiNoti[rigaPivot] == 0 ? 0 : coefficientiNoti[rigaPivot] / valorePivot;
+            }
+        }
+        // riduzione delle altre 2 righe per ottenere un vettore linearmente indipendente (porto a zero i valori aventi
+        // la stessa colonna del pivot)
+        for (int i = 0; i < matrice.length; i++) {
+            if (i == rigaPivot) //questo procedimento non va ovviamente eseguito per la riga del pivot
+                continue;
+            valoreMoltiplicativo = matrice[i][colonnaPivot] / 1; // diviso 1 per sapere se mettere il segno meno
+            for (int j = 0; j < matrice[i].length; j++) {
+                matrice[i][j] = matrice[i][j] - valoreMoltiplicativo * matrice[rigaPivot][j];
+                coefficientiNoti[i] = coefficientiNoti[i] - valoreMoltiplicativo * coefficientiNoti[rigaPivot];
+            }
+        }
+
+        this.aggiornaFunzioneObbiettivo();
     }
 
-    if (elemento[0] > -1 && elemento[1] > -1) {
-      System.out.println("riga " + elemento[0] + " colonna " + elemento[1]);
-      System.out.println("pivot " + matrice[elemento[0]][elemento[1]]);
+    public void aggiornaFunzioneObbiettivo() {
+        //TODO:da scrivere
     }
 
-    return elemento;
-  }
+    /*TODO: da testare*/
+    public int[] trovaVariabileUscente() {
+        System.out.println("uno");
+        int[] elemento = {-1, trovaVariabileEntrante()};
 
-  public void stampaFunzioneObiettivo() {
-    char[] c = {'₁', '₂', '₃', '₄', '₅'};
+        if (elemento[1] > -1) {
+            System.out.println("colonna " + elemento[1]);
 
-    System.out.println("funzione obiettivo");
-    for (int i = 0; i < funzioneObiettivo.length; i++) {
-      if (funzioneObiettivo[i] != 0) {
-        System.out.print(nf.format(funzioneObiettivo[i]) + "x" + c[i] + " ");
-      }
+            double minimo = Double.MAX_VALUE;
+            for (int i = 0; i < matrice.length && elemento[1] > -1; i++) {
+                double valoreI = matrice[i][elemento[1]] / coefficientiNoti[i];
+                if (valoreI > 0 && valoreI < minimo) {
+                    System.out.println("i " + i + " valoreI " + valoreI);
+                    minimo = valoreI;
+                    elemento[1] = i;
+                }
+                System.out.println("minimo " + minimo);
+            }
+        }
+
+        if (elemento[0] > -1 && elemento[1] > -1) {
+            System.out.println("riga " + elemento[0] + " colonna " + elemento[1]);
+            System.out.println("pivot " + matrice[elemento[0]][elemento[1]]);
+        }
+
+        return elemento;
     }
-    System.out.println("\n");
-  }
 
-  public void stampaMatrice() {
-    stampaFunzioneObiettivo();
+    public void stampaFunzioneObiettivo() {
+        char[] c = {'₁', '₂', '₃', '₄', '₅'};
 
-    System.out.println("stampo la matrice");
-    for (int i = 0; i < matrice.length; i++) {
-      for (int j = 0; j < matrice[i].length; j++) {
-        System.out.print(nf.format(matrice[i][j]) + "\t");
-      }
-      System.out.print("| " + nf.format(coefficientiNoti[i]));
-      System.out.println();
+        System.out.println("funzione obiettivo");
+        for (int i = 0; i < funzioneObiettivo.length; i++) {
+            if (funzioneObiettivo[i] != 0) {
+                System.out.print(nf.format(funzioneObiettivo[i]) + "x" + c[i] + " ");
+            }
+        }
+        System.out.println("\n");
     }
-  }
+
+    public void stampaMatrice() {
+        stampaFunzioneObiettivo();
+
+        System.out.println("stampo la matrice");
+        for (int i = 0; i < matrice.length; i++) {
+            for (int j = 0; j < matrice[i].length; j++) {
+                System.out.print(nf.format(matrice[i][j]) + "\t");
+            }
+            System.out.print("| " + nf.format(coefficientiNoti[i]));
+            System.out.println();
+        }
+    }
 }
--- Simplesso.java
+++ Simplesso.java
@@ -8,77 +8,81 @@ import static java.lang.System.exit;
 import static java.util.stream.Collectors.toList;
 
 class Simplesso {
-  public static void main(String[] args) {
-    Matrice matrice;
-    boolean trovatoMassimo = false;
-    boolean illimitata = false;
-    boolean troppiCicli = false;
-    int counter = 0;
+    public static void main(String[] args) {
 
-    if (args.length == 0) {
-      System.out.println("non è stato specificato alcun argomento, procedo  tramite riga di comando\n");
-      matrice = new Matrice();
-    } else{
-      if (args[0].equals("-h")){
-        System.out.println("inserire come argomento il path del file contenente:\ncoefficienti della funzione obbiettivo\nmatrice dei coefficienti\n\n" +
-                "Esempio:\n2 1\n1 1 1 0 0 1\n1 3 0 1 0 6\n1 0 0 0 1 4\n");
-      }
-      matrice=fileParsing(args[0]);
+        /*
+        TEST PUSH
+         */
+        Matrice matrice;
+        boolean trovatoMassimo = false;
+        boolean illimitata = false;
+        boolean troppiCicli = false;
+        int counter = 0;
 
-    }
-    matrice.stampaMatrice();
+        if (args.length == 0) {
+            System.out.println("non è stato specificato alcun argomento, procedo  tramite riga di comando\n");
+            matrice = new Matrice();
+        } else {
+            if (args[0].equals("-h")) {
+                System.out.println("inserire come argomento il path del file contenente:\ncoefficienti della funzione obbiettivo\nmatrice dei coefficienti\n\n" +
+                        "Esempio:\n2 1\n1 1 1 0 0 1\n1 3 0 1 0 6\n1 0 0 0 1 4\n");
+            }
+            matrice = fileParsing(args[0]);
 
-    trovatoMassimo = matrice.trovaVariabileEntrante() == -1;
-
-    while (!trovatoMassimo && !illimitata && !troppiCicli) {
-        illimitata = matrice.trovaVariabileUscente()[0] == -1;
+        }
+        matrice.stampaMatrice();
 
         trovatoMassimo = matrice.trovaVariabileEntrante() == -1;
-        troppiCicli = ++counter == 2;
+
+        while (!trovatoMassimo && !illimitata && !troppiCicli) {
+            illimitata = matrice.trovaVariabileUscente()[0] == -1;
+
+            trovatoMassimo = matrice.trovaVariabileEntrante() == -1;
+            troppiCicli = ++counter == 2;
+        }
+
+        if (trovatoMassimo) System.out.println("trovata la soluzione");
+        else if (illimitata) System.out.println("il sistema é illimitato");
+        else if (troppiCicli) System.out.println("errore!");
+
     }
 
-    if (trovatoMassimo) System.out.println("trovata la soluzione");
-    else if (illimitata) System.out.println("il sistema é illimitato");
-    else if (troppiCicli) System.out.println("errore!");
+    static public Matrice fileParsing(String file) {
+        List<String> lines = null;
+        int i = 0;
+        String[] sub;
+        double[] knownCoeff = new double[3];
+        double[] targetFunction = new double[0];
+        double[][] matrice = new double[3][5];
 
-  }
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            lines = in.lines().collect(toList());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        if (lines == null) {
+            System.out.println("non riesco a leggere il file\n");
+            exit(1);
+        }
 
-  static public Matrice fileParsing(String file)  {
-    List<String> lines = null;
-    int i=0;
-    String []sub;
-    double[] knownCoeff =new double[3];
-    double[] targetFunction = new double[0];
-    double[][] matrice = new double[3][5];
+        for (String line : lines) {
+            if (i == 0) {
+                targetFunction = Arrays.stream(line.split(" ")).map(Double::valueOf).mapToDouble(Double::doubleValue).toArray();
+                i++;
+            } else {
+                sub = line.split(" ");
+                for (int j = 0; j < 5; j++)
+                    matrice[i - 1][j] = Double.parseDouble(sub[j]);
+                knownCoeff[i - 1] = Double.parseDouble(sub[5]);
+                i++;
+            }
+        }
+        if (i != 4) {
+            System.out.println("file male formattato:\ncoefficienti della funzione obbiettivo\nmatrice dei coefficienti\n\n" +
+                    "Esempio:\n2 1\n1 1 1 0 0 1\n1 3 0 1 0 6\n1 0 0 0 1 4\n");
+            exit(1);
+        }
 
-    try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-      lines = in.lines().collect(toList());
-    } catch (IOException e) {
-      System.err.println(e.getMessage());
+        return new Matrice(matrice, knownCoeff, targetFunction);
     }
-    if (lines == null) {
-      System.out.println("non riesco a leggere il file\n");
-      exit(1);
-    }
-
-    for (String line :lines) {
-      if (i==0) {
-        targetFunction = Arrays.stream(line.split(" ")).map(Double::valueOf).mapToDouble(Double::doubleValue).toArray();
-        i++;
-      }else {
-        sub=line.split(" ");
-        for (int j=0;j<5;j++)
-          matrice[i-1][j]=Double.parseDouble(sub[j]);
-        knownCoeff[i-1]=Double.parseDouble(sub[5]);
-        i++;
-      }
-    }
-    if (i!=4){
-      System.out.println("file male formattato:\ncoefficienti della funzione obbiettivo\nmatrice dei coefficienti\n\n" +
-              "Esempio:\n2 1\n1 1 1 0 0 1\n1 3 0 1 0 6\n1 0 0 0 1 4\n");
-      exit(1);
-    }
-
-    return new Matrice(matrice,knownCoeff,targetFunction);
-  }
 }
